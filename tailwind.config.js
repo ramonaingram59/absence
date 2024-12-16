a1b514aa0f55e4a6f4845024a7090cbad1a1fd59
rizeenf <rize.nf11@gmail.com>
2024-12-16 15:06:50 +0800

    feat: fix deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
-
-module.exports = {
+export default {
   darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -88,4 +86,4 @@ module.exports = {
     },
   },
   plugins: [require('tailwindcss-animate')],
-};
+}
